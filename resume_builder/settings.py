6194b1ad090a8a1306d56020be5650076978d28d
Krishanu <krishanusaha769@gmail.com>
2025-08-27 00:52:43 +0530

    Updated settings and static config

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x$y*9&x4td5+pe_-2=($7iqd&2e%a3+wm%u=*1i)t)(y0v2&t9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
