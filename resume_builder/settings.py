d990a5fd25792eec867a7d4876963775224c58e0
Krishanu <krishanusaha769@gmail.com>
2025-10-05 16:19:15 +0530

    allowed all host

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x$y*9&x4td5+pe_-2=($7iqd&2e%a3+wm%u=*1i)t)(y0v2&t9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
